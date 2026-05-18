55e5a11298e8a109dff5a7c3c9c3f20fc0c0cd72
19844842-cyber <19844842@clases.edu.sv>
2026-05-18 17:09:26 -0600

    archivo principal
    es el archivo principal donde estan todos los enlaces de los archivos

---
+++ indes.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>menu</title>
+</head>
+<body>
+    <h1>menu de ejercicios</h1>
+    <a href="mi nombre.html"><h2>ejercicio1</h2></a>
+    <a href="dos numeros.html"><h2>ejercicio2</h2></a>
+    <a href="menos dos numeros.html"><h2>ejercicio3</h2></a>
+    <a href="multiplica los numeros.html"><h2>ejercicio4</h2></a>
+    <a href="divide los numeros.html"><h2>ejercicico5</h2></a>
+    <a href="nombre y datos.html"><h2>ejercicio6</h2></a>
+    <a href="dos numeros mayores.html"><h2>ejercicio7</h2></a>
+    <a href="dos numeros menores.html"><h2>ejercicio8</h2></a>
+    <a href="par o impar.html"><h2>ejercicio9</h2></a>
+    <a href="10 notas.html"><h2>ejercio10</h2></a>
+    <a href="11 aprobado o reprobado.html"><h2>ejercicio11</h2></a>
+    <a href="base del rectangulo.html"><h2>ejercicio12</h2></a>
+    <a href="el cuadrado.html"><h2>ejercicio13</h2></a>
+    <a href="cantidad en metros.html"><h2>eercicio14</h2></a>
+    <a href="temperatura.html"><h2>ejercicio15</h2></a>
+    <a href="salario de trabajo.html"><h2>ejercicio16</h2></a>
+    <a href="total de compra.html"><h2>ejercicio17</h2></a>
+    <a href="datos de fac.html"><h2>ejercicio18</h2></a>
+    <a href="tabla de multiplicar.html"><h2>ejercicio19</h2></a>
+    <a href="1 hasta numero ingresado.html"><h2>ejercicio20</h2></a>
+    <a href="una palabra.html"><h2>ejercicio21</h2></a>
+    <a href="
