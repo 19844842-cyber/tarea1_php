6270e056bd488fe4b6cd57cdd5d6597c0bbfb012
19844842-cyber <19844842@clases.edu.sv>
2026-05-18 19:36:34 -0600

    ejercicio8
     aqui Diseñe un formulario que solicite dos números y envíe los datos a otro archivo
    PHP donde se determine y muestre cuál número es mayor.

---
+++ mi numero menor.php
@@ -0,0 +1,12 @@
+<?php
+$num1 = $_POST['numero1'];
+$num2 = $_POST['numero2'];
+
+if ($num1 < $num2) {
+    $nummenor = $num1;
+} else {
+    $nummenor = $num2;
+}
+
+echo "El número menor es: " . $nummenor;
+?>
